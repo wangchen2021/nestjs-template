aa600bb6f66a7fb5387885f6e1808dd6037a54df
chen <1710312281@QQ.COM>
2026-02-08 22:14:55 +0800

    feat: 28

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { AppDto } from './common/dto/app.dto';
 import { VersionService } from './common/services/version.service';
+import { AppConfigService } from './config/config.service';
 @Injectable()
 export class AppService {
-  constructor(private versionService: VersionService) {}
+  constructor(
+    private readonly versionService: VersionService,
+    private readonly appConfigService: AppConfigService,
+  ) {}
   getAppInfo(): AppDto {
     return {
+      app: this.appConfigService.getAppName(),
       version: this.versionService.getFullVersion(),
       env: this.versionService.getEnv(),
       data: this.versionService.getBuildTime(),
